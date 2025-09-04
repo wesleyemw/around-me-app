0392d3957e0e8b0731d4cc60266c4bec43efddd2
Wesley Soares <wesley.emw@gmail.com>
2025-09-04 14:16:46 -0400

    change style, basic css

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,12 @@ import 'maplibre-gl/dist/maplibre-gl.css';
 
 const map = new maplibregl.Map({
     container: 'map', // container id
-    style: 'https://demotiles.maplibre.org/globe.json', // style URL
-    center: [0, 0], // starting position [lng, lat]
-    zoom: 1 // starting zoom
+    style: 'https://tiles.openfreemap.org/styles/bright', // style URL
+    center: [-82.9833, 39.983334], // starting position [lng, lat]
+    zoom: 14 // starting zoom
+});
+
+map.on('load', async () => {
+    const image = await map.loadImage('https://maplibre.org/maplibre-gl-js/docs/assets/osgeo-logo.png');
+    map.addImage('custom-marker', image.data);
 });
