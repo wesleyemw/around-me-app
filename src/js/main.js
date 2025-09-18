13c27f230ac16387fa7e89ecac3baff68cf49322
Wesley Soares <wesley.emw@gmail.com>
2025-09-18 17:44:49 -0400

    get position api

--- src/js/main.js
+++ src/js/main.js
@@ -113,3 +113,27 @@ map.on('load', function () {
     let nc = new maplibregl.NavigationControl();
     map.addControl(nc, 'top-left');
 });
+
+
+// get current position
+
+const options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0,
+};
+
+function success(pos) {
+  const crd = pos.coords;
+
+  console.log("Your current position is:");
+  console.log(`Latitude : ${crd.latitude}`);
+  console.log(`Longitude: ${crd.longitude}`);
+  console.log(`More or less ${crd.accuracy} meters.`);
+}
+
+function error(err) {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+}
+
+navigator.geolocation.getCurrentPosition(success, error, options);
