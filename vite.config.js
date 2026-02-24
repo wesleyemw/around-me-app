74cd47371630435fe9ebca0e997fc618e831b46f
Wesley Soares <wesley.emw@gmail.com>
2026-02-24 16:44:35 -0500

    added biome basic config

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,25 @@
 import { dirname, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
+import biomePlugin from "vite-plugin-biome";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-	build: {
-		outDir: "dist",
-		rollupOptions: {
-			input: {
-				main: resolve(__dirname, "index.html"),
-				map: resolve(__dirname, "/pages/map/index.html"),
-			},
-		},
-	},
+  plugins: [
+    biomePlugin({
+      mode: "check",
+      files: "src/**/*.**",
+      applyFixes: "false",
+    }),
+  ],
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        map: resolve(__dirname, "/pages/map/index.html"),
+      },
+    },
+  },
 });
