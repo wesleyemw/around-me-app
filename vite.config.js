a3205ce3dbb3d662397eb3dd97680a18931cb1f1
Wesley Soares <wesley.emw@gmail.com>
2026-02-24 17:04:17 -0500

    updated packages

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,10 @@
 import { dirname, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
-import biomePlugin from "vite-plugin-biome";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  plugins: [
-    biomePlugin({
-      mode: "check",
-      files: "src/**/*.**",
-      applyFixes: "false",
-    }),
-  ],
   build: {
     outDir: "dist",
     rollupOptions: {
