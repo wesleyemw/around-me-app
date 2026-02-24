808e3a3d4d3b39a03f71c9bf064fad624ddb1768
Wesley Soares <wesley.emw@gmail.com>
2026-02-24 16:05:04 -0500

    transformed in a mpa application

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+import { defineConfig } from "vite";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  build: {
-    outDir: 'dist'
-  }
-})
+	build: {
+		outDir: "dist",
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, "index.html"),
+				map: resolve(__dirname, "/pages/map/index.html"),
+			},
+		},
+	},
+});
