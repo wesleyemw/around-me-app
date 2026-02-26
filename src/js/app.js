34bc4da1894ffc8e49f1d20e68d20dfe506d6433
Wesley Soares <wesley.emw@gmail.com>
2026-02-26 17:46:40 -0500

    added basic getdata function

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,19 @@
 (() => {
   window.app = {};
+  app = {
+    getData: async () => {
+      const query = "São Paulo";
+      const headers = {
+        "X-RapidAPI-Key": import.meta.env.VITE_RAPIDAPI_KEY,
+        "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+      };
+      const url = `https://wft-geo-db.p.rapidapi.com/v1/geo/places?types=CITY&namePrefix=${query}`;
+
+      const response = await fetch(url, { headers: headers });
+      const result = await response.json();
+
+      console.log(result);
+    },
+  };
+  app.getData();
 })();
